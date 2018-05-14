5daafa0d3c64ca38fa25f7161aa41578d1851151
William Park <wjypark94@gmail.com>
2018-05-13 23:29:50 -0700

    added users folder with users model and router

---
+++ users/models.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+mongoose.Promise = global.Promise;
+
+const UserSchema = mongoose.Schema({
+    username: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+  });
+
+UserSchema.methods.serialize = function() {
+    return {
+      username: this.username || '',
+    };
+  };
+
+UserSchema.methods.forAuthToken = function(){
+    return {
+        userName: this.userName,
+        _id: this._id
+    }
+}
+
+UserSchema.methods.validatePassword = function(password){
+    return bcrypt.compare(password, this.password);
+}
+
+UserSchema.statics.hashPassword = function(password){
+    return bcrypt.hash(password, 10);
+}
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = {User};
---
+++ users/router.js
@@ -0,0 +1,57 @@
+'use strict';
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+const {User} = require('./models');
+const jsonParser = bodyParser.json();
+
+// Post to register a new user
+router.post('/', jsonParser, (req, res) => {
+
+  let {username, password} = req.body;
+
+  return User.find({username})
+    .count()
+    .then(count => {
+      if (count > 0) {
+        // There is an existing user with the same username
+        return Promise.reject({
+          code: 422,
+          reason: 'ValidationError',
+          message: 'Username already taken',
+          location: 'username'
+        });
+      }
+      // If there is no existing user, hash the password
+      return User.hashPassword(password);
+    })
+    .then(hash => {
+      return User.create({
+        username,
+        password: hash,
+      });
+    })
+    .then(user => {
+      return res.status(201).json(user.serialize());
+    })
+    .catch(err => {
+      // Forward validation errors on to the client, otherwise give a 500
+      // error because something unexpected has happened
+      if (err.reason === 'ValidationError') {
+        return res.status(err.code).json(err);
+      }
+      res.status(500).json({code: 500, message: 'Internal server error'});
+    });
+});
+
+// Never expose all your users like below in a prod application
+// we're just doing this so we have a quick way to see
+// if we're creating users. keep in mind, you can also
+// verify this in the Mongo shell.
+router.get('/', (req, res) => {
+  return User.find()
+    .then(users => res.json(users.map(user => user.serialize())))
+    .catch(err => res.status(500).json({message: 'Internal server error'}));
+});
+
+module.exports = {router};
