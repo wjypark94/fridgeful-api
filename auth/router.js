a6bddcc73781a252ae157f50933899150e0300a3
William Park <wjypark94@gmail.com>
2018-05-13 23:25:22 -0700

    added auth folder with router and strategies

---
+++ auth/router.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const express = require("express");
+const passport = require("passport");
+const jwt = require("jsonwebtoken");
+const bodyParser = require("body-parser");
+
+const config = require("../config");
+const router = express.Router();
+
+
+const createAuthToken = function(user) {
+    return jwt.sign({user}, config.JWT_SECRET, {
+      subject: user.username,
+      expiresIn: config.JWT_EXPIRY,
+      algorithm: 'HS256'
+    });
+  };
+  
+  const localAuth = passport.authenticate('local', {session: false});
+  router.use(bodyParser.json());
+  
+  // The user provides a username and password to login
+  router.post('/', localAuth, (req, res) => {
+    const authToken = createAuthToken(req.user.serialize());
+    res.json({authToken, userId: req.user._id});
+  });
+  
+  const jwtAuth = passport.authenticate('jwt', {session: false});
+  
+  // The user exchanges a valid JWT for a new one with a later expiration
+  router.post('/refresh', jwtAuth, (req, res) => {
+    const authToken = createAuthToken(req.user);
+    res.json({authToken});
+  });
+  module.exports = {router};
