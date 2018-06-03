d6db0a18649c5809346ee975590dba3030575a07
William Park <wjypark94@gmail.com>
2018-06-02 22:19:37 -0700

    changed file

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 "use strict";
 
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/fridgeful';
-exports.PORT = process.env.PORT || 13765;
+exports.PORT = process.env.PORT || 8080;
 exports.CLIENT_ORIGIN = "http://localhost:3000";
 exports.JWT_SECRET = process.env.JWT_SECRET || 'secret';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
