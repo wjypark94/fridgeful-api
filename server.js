6d1fbcfc44aec96aa52789a2ab7ff0feb7ab292e
William Park <wjypark94@gmail.com>
2018-06-03 02:09:24 -0700

    added access control allow origin on server

--- server.js
+++ server.js
@@ -26,6 +26,18 @@ const jwtAuth = passport.authenticate('jwt', { session: false });
 app.use(bodyParser.json());
 app.use(morgan('common'));
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.send(204);
+  }
+ 
+  
+  next();
+});
+
 app.use(
   cors({
       origin: CLIENT_ORIGIN
