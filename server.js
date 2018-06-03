7f3302303d025cf09247c7c64926010dbaecc6e9
William Park <wjypark94@gmail.com>
2018-06-03 02:02:13 -0700

    removed unused code on server

--- server.js
+++ server.js
@@ -6,36 +6,25 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const passport = require('passport');
 
-mongoose.Promise = global.Promise;
 
 const { router: usersRouter } = require('./users');
 const { router: authRouter, localStrategy, jwtStrategy } = require('./auth');
+
 const {DATABASE_URL, PORT} = require('./config');
 const {CLIENT_ORIGIN} = require('./config');
+
+mongoose.Promise = global.Promise;
 
 const recipeRouter = require('./recipeRouter');
 const { Recipe } = require('./models');
 
 const app = express();
+const cors = require('cors');
 const jsonParser = bodyParser.json();
 const jwtAuth = passport.authenticate('jwt', { session: false });
 
 app.use(bodyParser.json());
 app.use(morgan('common'));
-
-const cors = require('cors');
-
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-  if (req.method === 'OPTIONS') {
-    return res.send(204);
-  }
- 
-  
-  next();
-});
 
 app.use(
   cors({
