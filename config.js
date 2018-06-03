bd3af02c8ada3b89423452291ffa943dd6f96f39
William Park <wjypark94@gmail.com>
2018-06-02 23:00:34 -0700

    fix typo

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/fridgeful';
 exports.PORT = process.env.PORT || 8080;
-exports.CLIENT_ORIGIN = "http://localhost:3000" || "https://mystifying-jepsen-0af500.netlify.com/";
+exports.CLIENT_ORIGIN = "http://localhost:9090" || "https://mystifying-jepsen-0af500.netlify.com/";
 exports.JWT_SECRET = process.env.JWT_SECRET || 'secret';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
